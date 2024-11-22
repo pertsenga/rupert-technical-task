18790ed71b7173373cfaa72ab94a3fdcd43f3649
Rupert Señga <pertsenga@gmail.com>
2024-11-22 14:48:38 +0800

    Fetch posts data from controller and pass it into the view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client as GuzzleClient;
 
 class PostController extends Controller
 {
+    const JSON_PLACEHOLDER_POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return view('posts.index');
+        // Laravel's Illuminate\Support\Facades\Http Facade uses Guzzle under the hood
+        // but since the Technical Task instructions require the use of Guzzle, we'll use it directly
+        $client = new GuzzleClient();
+        $response = $client->get($this::JSON_PLACEHOLDER_POSTS_URL);
+        $posts = json_decode($response->getBody(), true);
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
     }
 
     /**

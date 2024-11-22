809abbd01e39053c58c311a5f1ff5562f25e752f
Rupert Señga <pertsenga@gmail.com>
2024-11-22 14:30:38 +0800

    Add posts controller, route and view

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow rounded">
+                Hello world
+            </div>
+        </div>
+    </div>
+</x-app-layout>

5256cfa49a6485fbc7485b1e474e51ec5c6b1ac3
Rupert Señga <pertsenga@gmail.com>
2024-11-22 15:52:54 +0800

    Redirect root route to posts index route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/posts');
 
 Route::resource('posts', PostController::class, [
     'only' => ['index', 'create', 'store']
